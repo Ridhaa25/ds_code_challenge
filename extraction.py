b4d46b11df835a50dad8cb787f5f8c8abb39d247
Reedstar <ridhaa2503@gmail.com>
2024-03-02 12:09:51 +0200

    Initial commit

---
+++ extraction.py
@@ -0,0 +1,17 @@
+import boto3
+import datacompy
+import pandas
+import json
+import logging
+
+
+logging.basicConfig(filename='Extraction_performance.log', encoding='utf-8', level=logging.DEBUG)
+
+# logging.debug('This message should go to the log file')
+# logging.info('So should this11')
+# logging.warning('And this, too')
+# logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+
+# if __name__ == '__main__':
+#     pass
